0042a93add2d05579c96c5a59c2f6af827684448
sonu28gpt <98702109+sonu28gpt@users.noreply.github.com>
2025-01-12 00:09:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,33 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	n=Number(n);
+	let j=0;
+	let ans=[];
+	let temp=0;
+	let sum=0;
+	while(j<arr.length){
+		if(arr[j]>n){
+			j++;
+		}
+		while(j<arr.length && sum+arr[j]<=n){
+			temp.push(arr[j]);
+			sum+=arr[j];
+			j++;
+		}
+		if(j>arr.length){
+			if(temp.length)
+			ans.push(temp);
+			break;
+		}
+		if(temp.length);
+		ans.push(temp);
+		temp=[];
+		sum=0;
+		
+	}
+	alert(ans);
+	return ans;
 };
 
 const n = prompt("Enter n: ");
